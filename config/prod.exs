fb6b49f478976d8391e44b2e66868053eb808439
iDavi <odavi20527@gmail.com>
2024-12-28 10:44:49 -0300

    fixes prod config file

--- config/prod.exs
+++ config/prod.exs
@@ -6,10 +6,9 @@ config :linkat_api, LinkatApi.Repo,
   username: "postgres",
   database: "linkat_db",
   stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-  config :linkat_api, LinkatApiWeb.Endpoint,
+config :linkat_api, LinkatApiWeb.Endpoint,
   url: [
     host: System.get_env("HOST"),
     port: System.get_env("PORT"),
@@ -18,11 +17,7 @@ config :linkat_api, LinkatApi.Repo,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   server: true,
-  http: [port: String.to_integer(System.get_env("PORT") || "4000")],
-  https: [
-    port: String.to_integer(System.get_env("HTTPS_PORT") || "4043"),
-    cipher_suite: :strong
-  ]
+  http: [port: String.to_integer(System.get_env("PORT") || "4000")]
 
 config :logger,
   backends: [:console],
