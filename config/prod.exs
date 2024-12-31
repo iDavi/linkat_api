d4c2c1a51a27507447b03422f4172f31aadda827
iDavi <odavi20527@gmail.com>
2024-12-30 22:23:28 -0300

    remove port config from prod.exs since it appears in runtime.exs

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,6 @@ config :linkat_api, LinkatApiWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   server: true,
-  http: [port: String.to_integer(System.get_env("PORT") || "4000")],
   allow_cors: ["https://linkat.fun", "https://www.linkat.fun"]
 
 config :logger,
